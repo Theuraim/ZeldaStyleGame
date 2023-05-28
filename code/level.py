6861529fe43554faa202f487639f0ad5a166fdc4
Carlos Junior <carlosjunior026@hotmail.com>
2023-05-28 11:25:40 -0300

    feature: adds the map graphics

--- code/level.py
+++ code/level.py
@@ -1,5 +1,5 @@
-import pygame
-import settings
+import pygame, settings, os
+from support import *
 from tile import Tile
 from player import Player
 from worldmap import *
@@ -7,6 +7,8 @@ from debug import debug
 
 class Level:
     def __init__(self):
+        #get the path for the project
+        self.path = os.getcwd()
         # get the display surface
         self.display_surface = pygame.display.get_surface()
 
@@ -15,7 +17,7 @@ class Level:
         self.obstacle_sprites = pygame.sprite.Group()
 
         #Map setup
-        settings.WORLD_MAP = self.setDungeon()
+        #settings.WORLD_MAP = self.setDungeon()
         # sprite setup
         self.create_map()
 
@@ -41,14 +43,39 @@ class Level:
         return self.dungeon
 
     def create_map(self):
-        for row_index, row in enumerate(settings.WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * settings.TILESIZE
-                y = row_index * settings.TILESIZE
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
+        layout = {
+            'boundary': import_csv_layout(self.path + '/map/map_floorBlocks.csv'),
+            'grass': import_csv_layout(self.path + '/map/map_Grass.csv'),
+            'object': import_csv_layout(self.path + '/map/map_LargeObjects.csv')
+        }
+
+        graphics = {
+            'grass': import_folder(self.path + '/graphics/grass'),
+            'objects': import_folder(self.path + '/graphics/objects') 
+        }
+
+        for style, layout in layout.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * settings.TILESIZE
+                        y = row_index * settings.TILESIZE
+
+                        if style == 'boundary':
+                            Tile((x, y), [self.obstacle_sprites], sprite_type = 'invisible')
+                        if style == 'grass':
+                            #creates the grass tiles
+                            random_grass_image = random.choice(graphics['grass'])
+                            Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'grass', random_grass_image)
+
+                            
+                            pass
+                        if style == 'object':
+                            #create the object tiles
+                            surf = graphics['objects'][int(col)]
+                            Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
+                        
+        self.player = Player((2000,1430), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw the game
@@ -64,11 +91,20 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
+        self.path = os.getcwd()
+
+        #creating the floor
+        self.floor_surf = pygame.image.load(self.path + '/graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0, 0))
     
     def custom_draw(self, player):
         #getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_position = sprite.rect.topleft - self.offset
